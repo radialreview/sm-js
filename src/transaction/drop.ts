b1d79f6e0443f3bcc3006959de988e17c9ab3b93
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2021-12-13 15:44:12 -0600

    drop

---
+++ src/transaction/drop.ts
@@ -0,0 +1,42 @@
+import { DocumentNode, gql } from '@apollo/client/core';
+import { getMutationNameFromOperations } from './getMutationNameFromOperations';
+
+export type DropNodeOperation = {
+  type: 'dropNode';
+  id: string;
+  name?: string;
+};
+
+export function dropNode(
+  operation: Omit<DropNodeOperation, 'type'>
+): DropNodeOperation {
+  return {
+    type: 'dropNode',
+    ...operation,
+  };
+}
+
+export function getMutationsFromTransactionDropOperations(
+  operations: Array<DropNodeOperation>
+): Array<DocumentNode> {
+  const allDropNodeOperations: Array<{
+    id: string;
+    name?: string;
+  }> = operations.map(operation => {
+    if (operation.type === 'dropNode') {
+      return operation;
+    } else {
+      throw Error(`Operation not recognized: "${operation}"`);
+    }
+  });
+
+  return allDropNodeOperations.map(operation => {
+    const name = getMutationNameFromOperations([operation], 'DropNode');
+
+    return gql`
+            mutation ${name} {
+                DropNode(nodeId: "${operation.id}")
+            }    
+        `;
+  });
+}
