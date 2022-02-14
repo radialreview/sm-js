f6a9f6a8f6bf5c63e2d75d9438114b9055de87e3
Brendan Bowidas <brendanbowidas@gmail.com>
2022-02-14 15:04:11 -0500

    MM-22: prep for use in MM repo (#13)
    * bugfixes
    
    * include dist in git
    
    * package.json update experiment

---
+++ dist/index.d.ts
@@ -0,0 +1,24 @@
+import { ISMJS, SMConfig, ISMData, SMDataDefaultFn, NodeRelationalQueryBuilderRecord, NodeMutationFn, NodeDefArgs, ISMNode } from './types';
+export * from './smDataTypes';
+export * from './react';
+export * from './config';
+export declare class SMJS implements ISMJS {
+    gqlClient: ISMJS['gqlClient'];
+    plugins: ISMJS['plugins'];
+    query: ISMJS['query'];
+    subscribe: ISMJS['subscribe'];
+    SMQueryManager: ISMJS['SMQueryManager'];
+    transaction: ISMJS['transaction'];
+    tokens: Record<string, string>;
+    DOFactory: ISMJS['DOFactory'];
+    DOProxyGenerator: ISMJS['DOProxyGenerator'];
+    constructor(config: SMConfig);
+    def<TNodeData extends Record<string, ISMData | SMDataDefaultFn>, TNodeComputedData extends Record<string, any> = {}, TNodeRelationalData extends NodeRelationalQueryBuilderRecord = {}, TNodeMutations extends Record<string, NodeMutationFn> = {}>(def: NodeDefArgs<TNodeData, TNodeComputedData, TNodeRelationalData, TNodeMutations>): ISMNode<TNodeData, TNodeComputedData, TNodeRelationalData, TNodeMutations>;
+    getToken(opts: {
+        tokenName: string;
+    }): string;
+    setToken(opts: {
+        tokenName: string;
+        token: string;
+    }): void;
+}
