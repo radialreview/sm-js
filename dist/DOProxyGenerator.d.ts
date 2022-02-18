af267c19d189734953279cf73443a836a80d81e4
Brendan Bowidas <brendanbowidas@gmail.com>
2022-02-18 12:59:27 -0500

    remove dist

--- dist/DOProxyGenerator.d.ts
+++
@@ -1,9 +0,0 @@
-import { ISMJS, ISMData, SMDataDefaultFn, IDOProxy, ISMNode, NodeDO, Maybe, RelationalQueryRecordEntry } from './types';
-export declare function createDOProxyGenerator(smJSInstance: ISMJS): <TNodeData extends Record<string, ISMData<any, any, any> | SMDataDefaultFn>, TNodeComputedData extends Record<string, any>, TRelationalResults extends Record<string, IDOProxy | IDOProxy[]>>(opts: {
-    node: ISMNode<TNodeData, TNodeComputedData, {}, {}, NodeDO>;
-    queryId: string;
-    do: NodeDO;
-    allPropertiesQueried: Array<string>;
-    relationalResults: Maybe<TRelationalResults>;
-    relationalQueries: Maybe<Record<string, RelationalQueryRecordEntry>>;
-}) => Record<string, any> & import("./types").IDOMethods & TRelationalResults & IDOProxy;
--- dist/transaction/types.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type NodeData = {
-    type?: string;
-    childNodes?: Array<NodeData>;
-} & Record<string, any>;
