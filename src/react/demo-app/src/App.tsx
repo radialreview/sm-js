49baab639e664ef1a72282a8ae76d45903b4d5f1
Brendan Bowidas <brendanbowidas@gmail.com>
2022-02-22 12:41:16 -0500

    updated demo app

--- src/react/demo-app/src/App.tsx
+++ src/react/demo-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { useSubscription, queryDefinition } from 'sm-js';
-import { userNode, todoNode } from './smJS';
+import { useSubscription, queryDefinition, NodeData } from 'sm-js';
+import smJS, { userNode, todoNode } from './smJS';
 
 function MyComponent() {
   const { data } = useSubscription({
@@ -22,10 +22,77 @@ function MyComponent() {
     }),
   });
 
+  const createUser = async (opts: {
+    firstName: string;
+    lastName: string;
+    address: string;
+  }) => {
+    return smJS
+      .transaction(ctx => {
+        ctx.createNode({
+          data: {
+            type: userNode.type,
+            firstName: opts.firstName,
+            lastName: opts.lastName,
+            address: opts.address,
+          },
+        });
+      })
+      .execute();
+  };
+
+  const createTodo = async (opts: { task: string; assigneeId?: string }) => {
+    const createNodeData: NodeData = {
+      type: todoNode.type,
+      task: opts.task,
+    };
+
+    if (opts.assigneeId) {
+      createNodeData.assigneeId = opts.assigneeId;
+      createNodeData.additionalEdges = [
+        {
+          from: opts.assigneeId,
+          view: true,
+          edit: true,
+          manage: true,
+        },
+      ];
+    }
+
+    return smJS
+      .transaction(ctx => {
+        ctx.createNode({ data: createNodeData });
+      })
+      .execute();
+  };
+
+  const handleCreateUser = async () => {
+    await createUser({
+      firstName: `user_${(data.users || []).length}`,
+      lastName: 'Jones',
+      address: '123 Main St.',
+    });
+  };
+
+  const handleCreateTodo = async (assigneeId: string) => {
+    await createTodo({
+      task: 'Do this thing',
+      assigneeId,
+    });
+  };
+
   return (
     <div className="App">
       {data.users.map(user => (
-        <div key={user.id}>
+        <div
+          key={user.id}
+          style={{
+            border: '1px solid',
+            marginBottom: '16px',
+            padding: '8px',
+            width: '200px',
+          }}
+        >
           {user.firstName} {user.lastName}
           <br />
           todos:{' '}
@@ -34,8 +101,12 @@ function MyComponent() {
             : 'None.'}
           <br />
           <br />
+          <button onClick={() => handleCreateTodo(user.id)}>Add Todo</button>
         </div>
       ))}
+      <button onClick={handleCreateUser}>Create User</button>
+      <br />
+      <br />
     </div>
   );
 }
@@ -46,6 +117,7 @@ function App() {
   return (
     <>
       {showData && <MyComponent />}
+
       <button onClick={() => setShowData(showData => !showData)}>
         Toggle showing data
       </button>
--- src/react/demo-app/src/smJS.ts
+++ src/react/demo-app/src/smJS.ts
@@ -3,9 +3,10 @@ import {
   getDefaultConfig,
   SMJS,
   string,
-  // reference,
-  // IByReferenceQueryBuilder,
-  // ISMNode,
+  reference,
+  IByReferenceQueryBuilder,
+  IChildrenQueryBuilder,
+  ISMNode,
 } from 'sm-js';
 
 const smJS = new SMJS(getDefaultConfig());
@@ -13,7 +14,7 @@ const smJS = new SMJS(getDefaultConfig());
 smJS.setToken({
   tokenName: 'default',
   token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImY5ZGIxYjA5LTdiMGItNDMzYy1iZDg2LTg5M2MzZmQxNDFmMCIsIk5hbWVzcGFjZUFwcGxpY2F0aW9uSWQiOiIyIiwibmJmIjoxNjQ0NDM4NDg4LCJleHAiOjE2NDQ1MjQ4ODgsImlhdCI6MTY0NDQzODQ4OH0.kt1mR159rr7r4YlABL3k_t7vr_ZnzAz-UUdX6hUYSDg',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjRjYmNjMzA3LTZiZTMtMDdlMy1hOGJjLTJmOTBkZDZkNjUzNCIsIk5hbWVzcGFjZUFwcGxpY2F0aW9uSWQiOiIyIiwibmJmIjoxNjQ1NTUwNjMxLCJleHAiOjE2NDU2MzcwMzEsImlhdCI6MTY0NTU1MDYzMX0.Tk_59TCwefh5feKVdLBgba_AtxdUgthYiG7erF-4qI0',
 });
 
 export default smJS;
@@ -23,26 +24,47 @@ const todoProperties = {
   task: string,
   assigneeId: string.optional,
 };
-export const todoNode = smJS.def({
+
+const userProperties = {
+  id: string,
+  firstName: string,
+  lastName: string,
+  address: string,
+};
+
+type TodoProperties = typeof todoProperties;
+
+type TodoRelationalData = {
+  assignee: IByReferenceQueryBuilder<UserNode>;
+};
+
+type TodoMutations = {};
+
+type TodoNode = ISMNode<TodoProperties, {}, TodoRelationalData, TodoMutations>;
+
+type UserProperties = typeof userProperties;
+
+type UserRelationalData = {
+  todos: IChildrenQueryBuilder<TodoNode>;
+};
+
+type UserNode = ISMNode<UserProperties, {}, UserRelationalData, {}>;
+
+export const todoNode: TodoNode = smJS.def({
   type: 'todo',
   properties: todoProperties,
-  // relational: {
-  //   assignee: () =>
-  //     reference<any, any>({
-  //       def: userNode,
-  //       idProp: 'assigneeId',
-  //     }),
-  // },
+  relational: {
+    assignee: () =>
+      reference<TodoNode, UserNode>({
+        def: userNode,
+        idProp: 'assigneeId',
+      }),
+  },
 });
 
-export const userNode = smJS.def({
-  type: 'tt-user',
-  properties: {
-    id: string,
-    firstName: string,
-    lastName: string,
-    address: string,
-  },
+export const userNode: UserNode = smJS.def({
+  type: 'demo-user',
+  properties: userProperties,
   relational: {
     todos: () => relational({ def: todoNode, name: 'assigned' }),
   },
