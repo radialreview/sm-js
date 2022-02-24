f7eb21d8ff4c8aacc9fe75257e5d12e52b8495ee
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2022-02-24 13:47:14 -0600

    auto generated build from post-commit

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
-import { ISMJS, SMConfig, ISMData, SMDataDefaultFn, NodeRelationalQueryBuilderRecord, NodeMutationFn, NodeDefArgs, ISMNode, SMDataEnum, MapFnForNode, QueryDefinition, QueryFilter, GetExpectedNodeDataType, DeepPartial, QueryDefinitionTarget, IChildrenQuery, IByReferenceQuery } from './types';
-export { ISMNode, SMDataEnum, MapFnForNode, QueryFilter, QueryDefinition, ISMJS, GetExpectedNodeDataType, DeepPartial, QueryDefinitionTarget, IChildrenQuery, IByReferenceQuery, };
+import { ISMJS, SMConfig, ISMData, SMDataDefaultFn, NodeRelationalQueryBuilderRecord, NodeMutationFn, NodeDefArgs, ISMNode, SMDataEnum, MapFnForNode, QueryDefinition, QueryFilter, GetExpectedNodeDataType, DeepPartial, QueryDefinitionTarget, IChildrenQuery, IChildrenQueryBuilder, IByReferenceQuery, IByReferenceQueryBuilder } from './types';
+export { ISMNode, SMDataEnum, MapFnForNode, QueryFilter, QueryDefinition, ISMJS, GetExpectedNodeDataType, DeepPartial, QueryDefinitionTarget, IChildrenQuery, IChildrenQueryBuilder, IByReferenceQuery, IByReferenceQueryBuilder, };
 export * from './smDataTypes';
 export * from './react';
 export * from './config';
