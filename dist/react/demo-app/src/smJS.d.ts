20522029207c5a68818353c3c84693df97876057
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2022-03-15 15:58:49 -0500

    auto generated build from post-commit

--- dist/react/demo-app/src/smJS.d.ts
+++ dist/react/demo-app/src/smJS.d.ts
@@ -2,3 +2,7 @@ declare const smJS: any;
 export default smJS;
 export declare const todoNode: any;
 export declare const userNode: any;
+export declare function authenticate(opts: {
+    username: string;
+    password: string;
+}): Promise<any>;
