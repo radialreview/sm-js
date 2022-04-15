bfa82505003568da8e7d222c7998d6a10fb8ef52
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2022-04-15 13:33:02 -0500

    auto generated build from post-commit

--- dist/react/demo-app/src/smJS.d.ts
+++ dist/react/demo-app/src/smJS.d.ts
@@ -1,7 +1,96 @@
-declare const smJS: any;
+import { SMJS } from 'sm-js';
+declare const smJS: SMJS;
 export default smJS;
-export declare const todoNode: any;
-export declare const userNode: any;
+export declare const todoNode: import("sm-js").ISMNode<"todo", {
+    id: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+    task: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+}, {}, {}, {}, import("sm-js/dist/types").NodeComputedFns<{
+    id: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+    task: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+}, {}>, import("sm-js/dist/types").NodeDO>;
+export declare const userNode: import("sm-js").ISMNode<"tt-user", {
+    id: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+    firstName: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+    lastName: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+    address: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+}, {}, {
+    todos: import("sm-js").IChildrenQueryBuilder<import("sm-js").ISMNode<"todo", {
+        id: {
+            <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+            _default: import("sm-js").SMData<"", "", undefined>;
+            optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+        };
+        task: {
+            <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+            _default: import("sm-js").SMData<"", "", undefined>;
+            optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+        };
+    }, {}, {}, {}, import("sm-js/dist/types").NodeComputedFns<{
+        id: {
+            <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+            _default: import("sm-js").SMData<"", "", undefined>;
+            optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+        };
+        task: {
+            <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+            _default: import("sm-js").SMData<"", "", undefined>;
+            optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+        };
+    }, {}>, import("sm-js/dist/types").NodeDO>>;
+}, {}, import("sm-js/dist/types").NodeComputedFns<{
+    id: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+    firstName: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+    lastName: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+    address: {
+        <TStringType extends string = string>(defaultValue: TStringType): import("sm-js").SMData<TStringType, TStringType, undefined>;
+        _default: import("sm-js").SMData<"", "", undefined>;
+        optional: import("sm-js").SMData<import("sm-js/dist/types").Maybe<string>, import("sm-js/dist/types").Maybe<string>, undefined>;
+    };
+}, {}>, import("sm-js/dist/types").NodeDO>;
 export declare function authenticate(opts: {
     username: string;
     password: string;
