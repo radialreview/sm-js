b9f7e275481a98a4a21b29fbec2e530a35c7dce2
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2022-04-18 13:08:34 -0500

    e2e test replicating issue

--- src/e2e.spec.ts
+++ src/e2e.spec.ts
@@ -1901,6 +1901,76 @@ test(
   TIMEOUT_MS
 );
 
+test.only(
+  'child data works and updates in real time',
+  async done => {
+    const { smJSInstance, mockTodoDef } = await getReferenceTestUtils();
+
+    const mockTodoGroupNode = smJSInstance.def({
+      type: 'mock-todo-group',
+      properties: {},
+      relational: {
+        todos: () => children({ def: mockTodoDef }),
+      },
+    });
+
+    const response = await smJSInstance
+      .transaction(ctx => {
+        ctx.createNode<typeof mockTodoGroupNode>({
+          data: {
+            type: mockTodoGroupNode.type,
+          },
+        });
+      })
+      .execute();
+
+    const mockTodoGroupNodeId = response[0].data.CreateNodes[0].id;
+
+    let iteration = 0;
+    const { unsub } = await smJSInstance.subscribe(
+      {
+        todoGroup: queryDefinition({
+          def: mockTodoGroupNode,
+          target: {
+            id: mockTodoGroupNodeId,
+          },
+          map: ({ todos }) => ({
+            todos: todos({
+              map: allData => allData,
+            }),
+          }),
+        }),
+      },
+      {
+        onData: ({ results }) => {
+          if (iteration === 0) {
+            expect(results.todoGroup.todos.length).toBe(0);
+            // create todo under the group
+
+            smJSInstance
+              .transaction(ctx => {
+                ctx.createNode({
+                  data: {
+                    type: mockTodoDef.type,
+                  },
+                  under: mockTodoGroupNodeId,
+                });
+              })
+              .execute();
+          } else if (iteration === 1) {
+            expect(results.todoGroup.todos.length).toBe(1);
+            expect(results.todoGroup.todos[0].id).toBeInstanceOf(String);
+            unsub();
+            done();
+          }
+          iteration++;
+        },
+      }
+    );
+  },
+  TIMEOUT_MS
+);
+
 async function getToken(opts: {
   authUrl: string;
   email: string;
