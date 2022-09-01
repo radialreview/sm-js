91d91fc1ef3b1e2ea5682eca500131ca12d89219
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2022-09-01 11:28:17 -0500

    auto generated build from post-commit

--- dist/react/demo-app/src/mmGQL.d.ts
+++
@@ -1,8 +0,0 @@
-declare const mmGQL: any;
-export default mmGQL;
-export declare const todoNode: any;
-export declare const userNode: any;
-export declare function authenticate(opts: {
-    username: string;
-    password: string;
-}): Promise<any>;
