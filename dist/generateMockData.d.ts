7312759da29ca178a52347bb2d58ab4881b1b9ec
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2022-09-13 17:01:23 -0500

    pagination wip

--- dist/generateMockData.d.ts
+++
@@ -1,6 +0,0 @@
-import { IData, QueryDefinitions, DataDefaultFn } from './types';
-export declare function getMockValuesForIDataRecord(record: Record<string, IData | DataDefaultFn>): Record<string, any>;
-export declare function generateMockNodeDataFromQueryDefinitions<TSMNode, TMapFn, TQueryDefinitionTarget, TQueryDefinitions extends QueryDefinitions<TSMNode, TMapFn, TQueryDefinitionTarget>>(opts: {
-    queryDefinitions: TQueryDefinitions;
-    queryId: string;
-}): Record<string, any>;
--- dist/gqlClient.d.ts
+++
@@ -1,7 +0,0 @@
-import { IGQLClient } from './types';
-interface IGetGQLClientOpts {
-    httpUrl: string;
-    wsUrl: string;
-}
-export declare function getGQLCLient(gqlClientOpts: IGetGQLClientOpts): IGQLClient;
-export {};
