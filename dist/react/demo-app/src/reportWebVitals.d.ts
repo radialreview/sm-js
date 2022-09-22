509b42e1efe33fbff0fe27972d69086b8b1a38fc
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2022-09-22 15:55:28 -0500

    auto generated build from post-commit

--- dist/react/demo-app/src/reportWebVitals.d.ts
+++ dist/react/demo-app/src/reportWebVitals.d.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 import { ReportHandler } from 'web-vitals';
 declare const reportWebVitals: (onPerfEntry?: ReportHandler | undefined) => void;
 export default reportWebVitals;
-=======
-declare const reportWebVitals: (onPerfEntry?: any) => void;
-export default reportWebVitals;
->>>>>>> origin/mm-gql
