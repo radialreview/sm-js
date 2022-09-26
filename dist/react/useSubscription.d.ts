bec42bfb5e1e81e84a1f457af9d4e9e9d374dc58
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2022-09-26 10:04:26 -0500

    delete dist

--- dist/react/useSubscription.d.ts
+++
@@ -1,4 +0,0 @@
-import { UseSubscriptionReturn, UseSubscriptionQueryDefinitions, UseSubscriptionQueryDefinitionOpts } from '../types';
-export declare function useSubscription<TNode, TMapFn, TQueryDefinitionTarget, TUseSubscriptionQueryDefinitionOpts extends UseSubscriptionQueryDefinitionOpts, TQueryDefinitions extends UseSubscriptionQueryDefinitions<TNode, TMapFn, TQueryDefinitionTarget, TUseSubscriptionQueryDefinitionOpts>>(queryDefinitions: TQueryDefinitions, opts?: {
-    subscriptionId?: string;
-}): UseSubscriptionReturn<TQueryDefinitions>;
