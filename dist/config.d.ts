94f07ca2c466d3d49097f479ff5ee2a416c075f3
Ricardo Marques <ricardoalmmarques+github@gmail.com>
2023-05-03 11:23:39 -0500

    auto generated build from post-commit

---
+++ dist/config.d.ts
@@ -0,0 +1,2 @@
+import { Config } from './types';
+export declare function getDefaultConfig(): Config;
---
+++ dist/queriers/generateMockDataUtilities.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="chance" />
+declare const chance: Chance.Chance;
+export declare function generateRandomString(): string;
+export declare function generateRandomBoolean(): boolean;
+export declare function generateRandomNumber(min: number, max: number): number;
+export declare function generateRandomId(): string;
+export { chance };
